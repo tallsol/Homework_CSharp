40b7e59f3ec7707342f1173e5d65729b6bbf7eb8
DESKTOP-FMCRHGJ\talls <tallsol@mail.ru>
2022-08-08 16:25:28 +0300

    Homework №1

---
+++ Homework_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+	{
+		Console.WriteLine("Большее число - " + a);
+        Console.WriteLine("Меньшее число - " + b);
+	}
+else
+	{
+		Console.WriteLine("Большее число - " + b);
+        Console.WriteLine("Меньшее число - " + a);
+	}
---
+++ Homework_02/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int result;
+
+if (a > b)
+	{
+		result = a;
+	}
+else
+	{
+        result = b;
+	}
+
+if (result > c)
+    {
+        Console.WriteLine("Наибольшее число - " + result);
+    }
+else
+    {
+        Console.WriteLine("Наибольшее число - " + c);
+    }
