44c3893c0b64c6915658bbe6c8005d619fc235dc
DESKTOP-FMCRHGJ\talls <tallsol@mail.ru>
2022-08-30 13:56:52 +0300

    Homework 6

---
+++ Homework_17/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223 -> 3
+
+
+Console.WriteLine("Введите массив: ");
+int[] array = new int[5];
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Массив: [{String.Join("; ", array)}]");
+
+
+for (int i = 0; i < array.Length; i++)
+{
+	if (array[i] > 0) count++;
+}
+
+Console.WriteLine($"Количество введённых чисел больше нуля - {count}");
---
+++ Homework_18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// Значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2:");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+// x = (y - b2) / k2 - выводим х из второго уравнения и вставляем его в первое уравнение
+
+double y = ((k1 * b2) - (k2 * b1)) / (k1 - k2); // получившаяся формула - результат сокращений
+double x = (((k1 * b2) - (k2 * b1)) / (k1 - k2) - b2) / k2; // вставляем y в уравнение с x
+
+
+Console.WriteLine($"Точка пересечения М: ({x};{y})");
